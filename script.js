f41cfe2da030caae72b16a1d10eccefee63fcea4
applaksina <ap.plaksina@gmail.com>
2026-07-09 16:32:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const btn = document.getElementById(‘toggleStyle’);
+const btn = document.getElementById('toggleStyle');
 
-btn.addEventListener(‘click’, () => {
-document.body.classList.toggle(‘style-fancy’);
+if (btn) {
+    btn.addEventListener('click', () => {
+        document.body.classList.toggle('style-fancy');
 
-if (document.body.classList.contains(‘style-fancy’)) {
-localStorage.setItem(‘style’, ‘fancy’);
-} else {
-localStorage.setItem(‘style’, ‘default’);
+        if (document.body.classList.contains('style-fancy')) {
+            localStorage.setItem('style', 'fancy');
+        } else {
+            localStorage.setItem('style', 'default');
+        }
+    });
 }
-});
 
 window.onload = () => {
-const saved = localStorage.getItem(‘style’);
-if (saved === ‘fancy’) {
-document.body.classList.add(‘style-fancy’);
-}
+    const saved = localStorage.getItem('style');
+    if (saved === 'fancy') {
+        document.body.classList.add('style-fancy');
+    }
 };
